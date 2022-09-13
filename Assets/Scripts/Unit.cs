f955ee571f0bc89e073134a800f12190ac5c2100
Corey Willis <coreydwillis@gmail.com>
2022-09-13 14:24:54 -0600

    Scene Data Persistence

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -23,10 +23,12 @@ public abstract class Unit : MonoBehaviour,
         m_Agent.acceleration = 999;
         m_Agent.angularSpeed = 999;
     }
-
     private void Start()
     {
-
+        if (MainManager.Instance != null)
+        {
+            SetColor(MainManager.Instance.TeamColor);
+        }
     }
 
     void SetColor(Color c)
